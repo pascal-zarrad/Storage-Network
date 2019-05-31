808fde1ff95859c6ba726c49311b199bd7525719
sam <samson.bassett@gmail.com>
2019-05-30 23:58:49 -0700

    1.7.6 #182 shift click hang

--- src/main/java/mrriegel/storagenetwork/block/cable/processing/ContainerCableProcessing.java
+++ src/main/java/mrriegel/storagenetwork/block/cable/processing/ContainerCableProcessing.java
@@ -2,11 +2,18 @@ package mrriegel.storagenetwork.block.cable.processing;
 
 import mrriegel.storagenetwork.block.cable.ContainerCable;
 import mrriegel.storagenetwork.block.cable.TileCable;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 
 public class ContainerCableProcessing extends ContainerCable {
 
   public ContainerCableProcessing(TileCable tile, InventoryPlayer playerInv) {
     super(tile, playerInv);
+  }
+
+  @Override
+  public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+    return ItemStack.EMPTY;
   }
 }
