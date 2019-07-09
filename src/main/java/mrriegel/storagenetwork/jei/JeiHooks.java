1e0b24f73cf9779429568c9f1f0897778796eb7c
sam <samson.bassett@gmail.com>
2019-07-09 07:53:43 -0700

    1.14 juke jei integration, no maven exists

--- src/main/java/mrriegel/storagenetwork/jei/JeiHooks.java
+++ src/main/java/mrriegel/storagenetwork/jei/JeiHooks.java
@@ -1,22 +1,21 @@
 package mrriegel.storagenetwork.jei;
+//import mezz.jei.api.recipe.IFocus;
+//import mezz.jei.config.KeyBindings;
+//import mezz.jei.gui.Focus;
 
-import mezz.jei.api.recipe.IFocus;
-import mezz.jei.config.KeyBindings;
-import mezz.jei.gui.Focus;
 import mrriegel.storagenetwork.StorageNetwork;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.common.Optional;
+//import net.minecraftforge.fml.common.Optional;
 
 public class JeiHooks {
 
   public static String getFilterText() {
     try {
       if (JeiSettings.isJeiLoaded()) {
-        return getJeiTextInternal();
+        return "";// getJeiTextInternal();
       }
     }
     catch (Exception e) {
-      StorageNetwork.instance.logger.error(" mezz.jei.Internal not found ", e);
+      StorageNetwork.LOGGER.error(" mezz.jei.Internal not found ", e);
     }
     return "";
   }
@@ -29,36 +28,31 @@ public class JeiHooks {
   public static void setFilterText(String s) {
     try {
       if (JeiSettings.isJeiLoaded()) {
-        setJeiTextInternal(s);
+        //        setJeiTextInternal(s);
       }
     }
     catch (Exception e) {
-      StorageNetwork.instance.logger.error(" mezz.jei.Internal not found ", e);
+      StorageNetwork.LOGGER.error(" mezz.jei.Internal not found ", e);
     }
   }
-
-  @Optional.Method(modid = "jei")
-  private static void setJeiTextInternal(String s) {
-    mezz.jei.Internal.getRuntime().getIngredientFilter().setFilterText(s);
-  }
-
-  @Optional.Method(modid = "jei")
-  private static String getJeiTextInternal() {
-    return mezz.jei.Internal.getRuntime().getIngredientFilter().getFilterText();
-  }
-
-  @Optional.Method(modid = "jei")
-  public static void testJeiKeybind(int keyCode, ItemStack stackUnderMouse) {
-    final boolean showRecipe = KeyBindings.showRecipe.isActiveAndMatches(keyCode);
-    final boolean showUses = KeyBindings.showUses.isActiveAndMatches(keyCode);
-    if (showRecipe || showUses) {
-      //      IClickedIngredient<?> clicked = getIngredientUnderMouseForKey(MouseHelper.getX(), MouseHelper.getY());
-      //      if (clicked != null) {
-      IFocus.Mode mode = showRecipe ? IFocus.Mode.OUTPUT : IFocus.Mode.INPUT;
-      mezz.jei.Internal.getRuntime().getRecipesGui().show(new Focus<ItemStack>(mode, stackUnderMouse));
-      //        clicked.onClickHandled();
-      //        return true;
-      //      }
-    }
-  }
+  //  @Optional.Method(modid = "jei")
+  //  private static void setJeiTextInternal(String s) {
+  //    mezz.jei.Internal.getRuntime().getIngredientFilter().setFilterText(s);
+  //  }
+  //
+  //  @Optional.Method(modid = "jei")
+  //  private static String getJeiTextInternal() {
+  //    return mezz.jei.Internal.getRuntime().getIngredientFilter().getFilterText();
+  //  }
+  //
+  //  @Optional.Method(modid = "jei")
+  //  public static void testJeiKeybind(int keyCode, ItemStack stackUnderMouse) {
+  //    final boolean showRecipe = KeyBindings.showRecipe.isActiveAndMatches(keyCode);
+  //    final boolean showUses = KeyBindings.showUses.isActiveAndMatches(keyCode);
+  //    if (showRecipe || showUses) {
+  //       IFocus.Mode mode = showRecipe ? IFocus.Mode.OUTPUT : IFocus.Mode.INPUT;
+  //      mezz.jei.Internal.getRuntime().getRecipesGui().show(new Focus<ItemStack>(mode, stackUnderMouse));
+  //
+  //    }
+  //  }
 }
