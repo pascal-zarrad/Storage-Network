ae6f3a93ec3cf8158f0a2d716426155c2f803c80
sam <samson.bassett@gmail.com>
2019-07-13 10:19:31 -0700

    voxel shape attempt

--- src/main/java/mrriegel/storagenetwork/block/cable/BlockCable.java
+++ src/main/java/mrriegel/storagenetwork/block/cable/BlockCable.java
@@ -8,25 +8,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ContainerBlock;
-import net.minecraft.block.FenceBlock;
-import net.minecraft.block.HugeMushroomBlock;
 import net.minecraft.block.material.Material;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.network.DebugPacketSender;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.ChestType;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
-import net.minecraft.world.IWorldReader;
-import net.minecraft.world.ServerWorld;
-import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraft.tileentity.TileEntity;
 
 import javax.annotation.Nullable;
 import java.util.Map;
@@ -42,17 +36,17 @@ public class BlockCable extends ContainerBlock {
 
     @Override
     public String getName() {
-      return this.name().toLowerCase();
+      return name().toLowerCase();
     }
   }
 
-  public static final EnumProperty DOWN = EnumProperty.create("down", EnumConnectType.class);
-  public static final EnumProperty UP = EnumProperty.create("up", EnumConnectType.class);
-  public static final EnumProperty NORTH = EnumProperty.create("north", EnumConnectType.class);
-  public static final EnumProperty SOUTH = EnumProperty.create("south", EnumConnectType.class);
-  public static final EnumProperty WEST = EnumProperty.create("west", EnumConnectType.class);
-  public static final EnumProperty EAST = EnumProperty.create("east", EnumConnectType.class);
-  public static final Map<Direction, EnumProperty> FACING_TO_PROPERTY_MAP = Util.make(Maps.newEnumMap(Direction.class), (p) -> {
+  private static final EnumProperty DOWN = EnumProperty.create("down", EnumConnectType.class);
+  private static final EnumProperty UP = EnumProperty.create("up", EnumConnectType.class);
+  private static final EnumProperty NORTH = EnumProperty.create("north", EnumConnectType.class);
+  private static final EnumProperty SOUTH = EnumProperty.create("south", EnumConnectType.class);
+  private static final EnumProperty WEST = EnumProperty.create("west", EnumConnectType.class);
+  private static final EnumProperty EAST = EnumProperty.create("east", EnumConnectType.class);
+  private static final Map<Direction, EnumProperty> FACING_TO_PROPERTY_MAP = Util.make(Maps.newEnumMap(Direction.class), (p) -> {
     p.put(Direction.NORTH, NORTH);
     p.put(Direction.EAST, EAST);
     p.put(Direction.SOUTH, SOUTH);
@@ -60,19 +54,74 @@ public class BlockCable extends ContainerBlock {
     p.put(Direction.UP, UP);
     p.put(Direction.DOWN, DOWN);
   });
+  private static VoxelShape AABB;
+  private static VoxelShape AABB_UP;
+  private static VoxelShape AABB_DOWN;
+  private static VoxelShape AABB_EAST;
+  private static VoxelShape AABB_WEST;
+  private static VoxelShape AABB_NORTH;
+  private static VoxelShape AABB_SOUTH;
 
   public BlockCable(String registryName) {
     super(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.2F));
-    this.setRegistryName(registryName);
-    this.setDefaultState(this.stateContainer.getBaseState()
+    setRegistryName(registryName);
+    setDefaultState(stateContainer.getBaseState()
         .with(NORTH, EnumConnectType.NONE).with(EAST, EnumConnectType.NONE)
         .with(SOUTH, EnumConnectType.NONE).with(WEST, EnumConnectType.NONE)
         .with(UP, EnumConnectType.NONE).with(DOWN, EnumConnectType.NONE));
-    HugeMushroomBlock x;
-    FenceBlock y;
   }
 
-  public BlockRenderType getRenderType(BlockState p_149645_1_) {
+  @Override
+  public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    makeShapes();
+    //    Block block = state.getBlock();
+    //
+    //    if (block == BOPBlocks.desert_grass)
+    //    {
+    //      return SHORT;
+    //    }
+    if (state.get(UP).equals(EnumConnectType.CABLE)) {
+      return AABB_UP;
+    }
+    if (state.get(DOWN).equals(EnumConnectType.CABLE)) {
+      return AABB_DOWN;
+    }
+    if (state.get(WEST).equals(EnumConnectType.CABLE)) {
+      return AABB_WEST;
+    }
+    if (state.get(EAST).equals(EnumConnectType.CABLE)) {
+      return AABB_EAST;
+    }
+    if (state.get(NORTH).equals(EnumConnectType.CABLE)) {
+      return AABB_NORTH;
+    }
+    if (state.get(SOUTH).equals(EnumConnectType.CABLE)) {
+      return AABB_SOUTH;
+    }
+    return AABB;
+  }
+
+  private static void makeShapes() {
+    double top = 16;
+    double bot = 0;
+    double C = 8;
+    double w = 3.5;
+    double sm = C - w;
+    double lg = C + w;
+    // xx, zz, yy
+    AABB = Block.makeCuboidShape(sm, lg, sm, lg, sm, lg);
+    //
+    AABB_UP = Block.makeCuboidShape(sm, lg, sm, lg, sm, top);
+    AABB_DOWN = Block.makeCuboidShape(sm, lg, sm, lg, bot, lg);
+    //
+    AABB_NORTH = Block.makeCuboidShape(sm, lg, bot, lg, sm, lg);
+    AABB_SOUTH = Block.makeCuboidShape(sm, lg, sm, top, sm, lg);
+    //
+    AABB_WEST = Block.makeCuboidShape(bot, lg, sm, lg, sm, lg);
+    AABB_EAST = Block.makeCuboidShape(sm, top, sm, lg, sm, lg);
+  }
+
+  @Override public BlockRenderType getRenderType(BlockState p_149645_1_) {
     return BlockRenderType.MODEL;
   }
 
@@ -86,24 +135,25 @@ public class BlockCable extends ContainerBlock {
     return new TileCable();
   }
 
-  public BlockState getExtendedState(BlockState state, IBlockReader world, BlockPos pos) {
+  @Override public BlockState getExtendedState(BlockState state, IBlockReader world, BlockPos pos) {
     return super.getExtendedState(state, world, pos);
   }
 
-  protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+  @Override protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
     super.fillStateContainer(builder);
     builder.add(UP, DOWN, NORTH, EAST, SOUTH, WEST);
   }
 
+  @Override
   public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld world, BlockPos currentPos, BlockPos facingPos) {
     EnumProperty property = FACING_TO_PROPERTY_MAP.get(facing);
     //TODO: api should come back here
     if (facingState.getBlock() instanceof BlockCable || facingState.getBlock() == ModBlocks.master
-       || facingState.getBlock() == ModBlocks.request){
+        || facingState.getBlock() == ModBlocks.request) {
       //dont set self to self
       return stateIn.with(property, EnumConnectType.CABLE);
     }
-    else if (isValidLinkNeighbor( stateIn,facing,facingState,world,currentPos,facingPos)
+    else if (isValidLinkNeighbor(stateIn, facing, facingState, world, currentPos, facingPos)
     ) {
       return stateIn.with(property, EnumConnectType.INVENTORY);
     }
@@ -112,7 +162,7 @@ public class BlockCable extends ContainerBlock {
     }
   }
 
-  protected boolean isValidLinkNeighbor(BlockState stateIn, Direction facing, BlockState facingState, IWorld world, BlockPos currentPos, BlockPos facingPos)  {
+  private static boolean isValidLinkNeighbor(BlockState stateIn, Direction facing, BlockState facingState, IWorld world, BlockPos currentPos, BlockPos facingPos) {
     if (facing == null) {
       return false;
     }
@@ -122,19 +172,15 @@ public class BlockCable extends ContainerBlock {
     TileEntity neighbor = world.getTileEntity(facingPos);
     if (neighbor != null
         && neighbor.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing.getOpposite()) != null
-    && stateIn.getBlock()  == ModBlocks.storagekabel) {
+        && stateIn.getBlock() == ModBlocks.storagekabel) {
       StorageNetwork.LOGGER.info("storage found " + neighbor + " DIR " + facing);
-
         StorageNetwork.LOGGER.info("storage got a direction");
         TileEntity myself  = world.getTileEntity(currentPos);
         if(myself instanceof TileCableLink){
           TileCableLink link=(TileCableLink) myself;
-
           StorageNetwork.LOGGER.info(" SET DIR " + facing );
           link.setDirection(facing);
-
-
-      }
+        }
       return true;
     }
     return false;
