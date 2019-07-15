2c08af194d861fb1de6bc31b810ffc867d199a67
sam <samson.bassett@gmail.com>
2019-07-15 16:33:58 -0700

    gui cable can open now

--- src/main/java/com/lothrazar/storagenetwork/setup/ClientProxy.java
+++ src/main/java/com/lothrazar/storagenetwork/setup/ClientProxy.java
@@ -1,5 +1,5 @@
 package com.lothrazar.storagenetwork.setup;
-
+import com.lothrazar.storagenetwork.block.cablefilter.GuiCableFilter;
 import com.lothrazar.storagenetwork.registry.SsnRegistry;
 import com.lothrazar.storagenetwork.block.request.GuiRequest;
 import net.minecraft.client.Minecraft;
@@ -9,20 +9,19 @@ import net.minecraft.world.World;
 
 public class ClientProxy implements IProxy {
 
+  @Override
+  public void init() {
+    ScreenManager.registerFactory(SsnRegistry.requestcontainer, GuiRequest::new);
+    ScreenManager.registerFactory(SsnRegistry.filterContainer, GuiCableFilter::new);
+  }
 
-	@Override
-	public void init()
-	{
-		ScreenManager.registerFactory(SsnRegistry.requestcontainer , GuiRequest::new);
-	}
+  @Override
+  public World getClientWorld() {
+    return Minecraft.getInstance().world;
+  }
 
-	@Override
-	public World getClientWorld() {
-		return Minecraft.getInstance().world;
-	}
-
-	@Override
-	public PlayerEntity getClientPlayer() {
-		return Minecraft.getInstance().player;
-	}
+  @Override
+  public PlayerEntity getClientPlayer() {
+    return Minecraft.getInstance().player;
+  }
 }
