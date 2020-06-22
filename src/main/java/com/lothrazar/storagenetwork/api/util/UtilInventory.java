1958a98ea425b14a77b5f32ec00e7445bd3f6f0a
lothrazar <samson.bassett@gmail.com>
2020-06-21 19:35:43 -0700

    1.0.0 bump. more removing unused

--- src/main/java/com/lothrazar/storagenetwork/api/util/UtilInventory.java
+++ src/main/java/com/lothrazar/storagenetwork/api/util/UtilInventory.java
@@ -1,48 +1,11 @@
 package com.lothrazar.storagenetwork.api.util;
 
 import com.lothrazar.storagenetwork.api.data.ItemStackMatcher;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemHandlerHelper;
-import net.minecraftforge.items.wrapper.InvWrapper;
-import net.minecraftforge.items.wrapper.SidedInvWrapper;
 
 public class UtilInventory {
-
-  public static boolean hasItemHandler(World world, BlockPos pos, Direction facing) {
-    return getItemHandler(world.getTileEntity(pos), facing) != null;
-  }
-
-  public static boolean doOverlap(String text, String name) {
-    return text.toLowerCase().contains(name.toLowerCase())
-        || name.toLowerCase().contains(text.toLowerCase());
-  }
-
-  public static IItemHandler getItemHandler(TileEntity tile, Direction side) {
-    if (tile == null) {
-      return null;
-    }
-    //    if (tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side)) {
-    IItemHandler cap = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side).orElse(null);
-    //    }
-    if (cap != null) {
-      return null;
-    }
-    if (tile instanceof ISidedInventory) {
-      return new SidedInvWrapper((ISidedInventory) tile, side);
-    }
-    if (tile instanceof IInventory) {
-      return new InvWrapper((IInventory) tile);
-    }
-    return null;
-  }
 
   public static String formatLargeNumber(int size) {
     if (size < Math.pow(10, 3)) {
@@ -62,17 +25,6 @@ public class UtilInventory {
       return rounded + "B";
     }
     return size + "";
-    //    String amount = size < 1000 ? String.valueOf(size) : size < 1000000 ? size / 1000 + "K" : size / 1000000 + "M";
-    //    return amount;
-  }
-
-  public static boolean contains(IItemHandler inv, ItemStack stack) {
-    for (int i = 0; i < inv.getSlots(); i++) {
-      if (ItemHandlerHelper.canItemStacksStack(inv.getStackInSlot(i), stack)) {
-        return true;
-      }
-    }
-    return false;
   }
 
   public static int containsAtLeastHowManyNeeded(IItemHandler inv, ItemStack stack, int minimumCount) {
@@ -87,20 +39,6 @@ public class UtilInventory {
       return 0;
     }
     return minimumCount - found;
-  }
-
-  public static int getAmount(IItemHandler inv, ItemStackMatcher fil) {
-    if (inv == null || fil == null) {
-      return 0;
-    }
-    int amount = 0;
-    for (int i = 0; i < inv.getSlots(); i++) {
-      ItemStack slot = inv.getStackInSlot(i);
-      if (fil.match(slot)) {
-        amount += slot.getCount();
-      }
-    }
-    return amount;
   }
 
   public static ItemStack extractItem(IItemHandler inv, ItemStackMatcher fil, int num, boolean simulate) {
